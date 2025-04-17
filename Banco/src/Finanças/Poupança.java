30d2fcc58f12462ed7e0fb9c1aadc00aea11a9f1
back-byte <ogabriel292@gmail.com>
2025-04-17 17:38:36 -0300

    Gerenciamento de conta bancária e caixa eletrônico, manipulando interface pela primeira vez, utilizando o Eclipse IDE.

---
+++ Banco/src/Finanças/Poupança.java
@@ -0,0 +1,72 @@
+package Finanças;
+
+public class Poupança extends Conta implements iOperacao {
+
+	public Poupança() {
+		super();
+	}
+
+	public Poupança(String numero, String agencia, double saldo) {
+		super(numero, agencia, saldo);
+	}
+
+	@Override
+	public void depositar(double saldoDepositar) {
+
+		if (saldoDepositar > 0.00) {
+
+			double saldoConta = getSaldo();
+
+			saldoConta += saldoDepositar;
+
+			setSaldo(saldoConta);
+
+			System.out.println("Você adicionou: R$" + saldoDepositar);
+		}
+
+		else {
+
+			System.out.println("Insira um valor maior que zero!");
+		}
+	}
+
+	@Override
+	public void sacar(double saldoASacar) {
+
+		if (saldoASacar > 0.00) {
+
+			if (saldoASacar > getSaldo()) {
+				System.out.println("Não há saldo suficiente!");
+			}
+
+			else {
+
+				double saldoSemTaxa, saldoConta;
+
+				saldoConta = getSaldo();
+
+				saldoSemTaxa = saldoASacar - (saldoASacar * 0.05);
+
+				saldoConta -= saldoSemTaxa;
+
+				setSaldo(saldoConta);
+
+				System.out.println("Você sacou " + saldoSemTaxa);
+			}
+		}
+
+		else {
+			System.out.println("Insira um valor maior que zero!");
+		}
+	}
+
+	@Override
+	public void exibirSaldo() {
+
+		double saldoConta = getSaldo();
+
+		System.out.println("\nSeu saldo é de: R$" + saldoConta);
+
+	}
+
+}
---
+++ Banco/src/Finanças/iOperacao.java
@@ -0,0 +1,12 @@
+package Finanças;
+
+public interface iOperacao {
+	
+	public void depositar(double saldoDepositar);
+
+	public void sacar(double saldoASacar);
+	
+	public void exibirSaldo();
+	
+	
+}
